99c6ade1043d8a1635ea14f523b085099cafba01
Silvi23707 <silviacastillo23707@gmail.com>
2024-09-10 14:24:54 -0600

    Update crud endpoints

--- backend/Agenda/src/main/java/com/romvo/agenda/adapter/in/rest/ContactoController.java
+++ backend/Agenda/src/main/java/com/romvo/agenda/adapter/in/rest/ContactoController.java
@@ -2,15 +2,22 @@ package com.romvo.agenda.adapter.in.rest;
 
 import com.romvo.agenda.adapter.in.rest.dto.ContactoDTO;
 import com.romvo.agenda.adapter.in.rest.dto.ContactoDTOMapper;
+import com.romvo.agenda.domain.entity.Contacto;
 import com.romvo.agenda.port.in.ServicioContacto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @RestController
@@ -28,5 +35,21 @@ public class ContactoController {
             .stream()
             .map(contactoDTOMapper::contactoToContactoDTO)
             .toList();
+    }
+
+    @PostMapping
+    public ContactoDTO crear(@RequestBody Contacto contacto) {
+        return contactoDTOMapper.contactoToContactoDTO(this.servicioContacto.crear(contacto));
+    }
+
+    @PutMapping()
+    public ContactoDTO editar( @RequestBody Contacto contacto) {
+        return contactoDTOMapper.contactoToContactoDTO(this.servicioContacto.editar(contacto));
+
+    }
+
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable Long id) {
+        this.servicioContacto.eliminar(id);
     }
 }
