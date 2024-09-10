a2425f64cf1135178605cf014266bf93e60463fc
Silvi23707 <silviacastillo23707@gmail.com>
2024-09-10 14:30:11 -0600

    Update ServicioContacto.java

--- backend/Agenda/src/main/java/com/romvo/agenda/port/in/ServicioContacto.java
+++ backend/Agenda/src/main/java/com/romvo/agenda/port/in/ServicioContacto.java
@@ -6,8 +6,9 @@ import com.romvo.agenda.domain.entity.Contacto;
 import java.util.List;
 
 public interface ServicioContacto{
+    Contacto findById(Long id);
     Contacto crear(Contacto contacto);
     Contacto editar(Contacto contacto);
-    void eliminar(Object id);
+    void eliminar(Long id);
     List<Contacto> listar();
 }
